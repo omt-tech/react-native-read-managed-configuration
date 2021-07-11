d3484fb3b698cd1a448cbdf79080142db694354c
Jin Park <jin@jinpark.net>
2021-07-11 19:04:43 +0800

    able to get array of restrictions

--- android/src/main/java/com/reactlibrary/ReadManagedConfigurationModule.java
+++ android/src/main/java/com/reactlibrary/ReadManagedConfigurationModule.java
@@ -4,7 +4,10 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
-
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.WritableArray;
+import com.facebook.react.bridge.WritableNativeArray;
 import com.facebook.react.bridge.WritableNativeMap;
 
 // import android.app.Activity;
@@ -29,22 +32,32 @@ public class ReadManagedConfigurationModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void getRestrictions(Callback callback) {
-        RestrictionsManager restrictionsManager =
-        (RestrictionsManager) this.reactContext.getSystemService(Context.RESTRICTIONS_SERVICE);
+    public void getRestrictions(ReadableArray restrictions, Promise promise) {
+        try {
+            RestrictionsManager restrictionsManager =
+            (RestrictionsManager) this.reactContext.getSystemService(Context.RESTRICTIONS_SERVICE);
 
-        if (restrictionsManager == null) {
-            Log.i("readmanaged", "no restrictions manager");
-            return;
+            if (restrictionsManager == null) {
+                Log.i("readmanaged", "no restrictions manager");
+                throw new RuntimeException("no restrictions manager");
+            }
+
+            Bundle appRestrictions = restrictionsManager.getApplicationRestrictions();
+            WritableArray restrictionsValues = new WritableNativeArray();
+
+            for (int i = 0; i < restrictions.size(); i++) {
+                if (appRestrictions.containsKey(restrictions.getString(i))) {
+                    // restrictionsValues[i] = appRestrictions.getString(restrictions[i]);
+                    restrictionsValues.pushString(appRestrictions.getString(restrictions.getString(i)));
+                } else {
+                    restrictionsValues.pushNull();
+                }
+            }
+            promise.resolve(restrictionsValues);
+        } catch(Exception e) {
+            promise.reject(e);
         }
         
-        Bundle appRestrictions = restrictionsManager.getApplicationRestrictions();
-        if (appRestrictions.containsKey("deviceid") && appRestrictions.containsKey("secretKey")) {
-                Log.i("readmanaged", "deviceid : " + appRestrictions.getString("deviceid"));
-                Log.i("readmanaged", "secretkey : " + appRestrictions.getString("secretKey"));
-
-                callback.invoke(appRestrictions.getString("deviceid"), appRestrictions.getString("secretKey"));
-        }
     }
 
     @ReactMethod
